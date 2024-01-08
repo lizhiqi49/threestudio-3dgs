e429b7106333f12612f94bae97477ede91c59e13
lizhiqi49 <zhqi.li@outlook.com>
2024-01-08 20:21:54 +0800

    temporal camera data module

--- data/temporal_image.py
+++ data/temporal_image.py
@@ -12,50 +12,49 @@ from torch.utils.data import DataLoader, Dataset, IterableDataset
 
 import threestudio
 from threestudio import register
-from threestudio.data.uncond import (
+from threestudio.utils.base import Updateable
+from threestudio.utils.config import parse_structured
+from threestudio.utils.misc import get_rank
+
+from threestudio.utils.typing import *
+
+from threestudio.data.image import (
+    SingleImageDataModuleConfig, 
+    SingleImageIterableDataset,
+    SingleImageDataset,
+)
+from .uncond import (
     RandomCameraDataModuleConfig,
     RandomCameraDataset,
     RandomCameraIterableDataset,
 )
-from threestudio.utils.base import Updateable
-from threestudio.utils.config import parse_structured
-from threestudio.utils.misc import get_rank
-from threestudio.utils.ops import (
-    get_mvp_matrix,
-    get_projection_matrix,
-    get_ray_directions,
-    get_rays,
-)
-from threestudio.utils.typing import *
 
+@dataclass 
+class TemporalRandomImageDataModuleConfig(SingleImageDataModuleConfig):
+    num_frames: int = 14
+    video_frames_dir: Optional[str] = None 
+    norm_timestamp: bool = False
 
-@dataclass
-class SingleImageDataModuleConfig:
-    # height and width should be Union[int, List[int]]
-    # but OmegaConf does not support Union of containers
-    height: Any = 96
-    width: Any = 96
-    resolution_milestones: List[int] = field(default_factory=lambda: [])
-    default_elevation_deg: float = 0.0
-    default_azimuth_deg: float = -180.0
-    default_camera_distance: float = 1.2
-    default_fovy_deg: float = 60.0
-    image_path: str = ""
-    use_random_camera: bool = True
-    random_camera: dict = field(default_factory=dict)
-    rays_noise_scale: float = 2e-3
-    batch_size: int = 1
-    requires_depth: bool = False
-    requires_normal: bool = False
+class TemporalRandomImageIterableDataset(IterableDataset, Updateable):
+    def __init__(self, cfg: Any, split: str) -> None:
+        super().__init__()
+        self.cfg = parse_structured(
+            TemporalRandomImageDataModuleConfig, cfg
+        )
+        self.num_frames = self.cfg.num_frames
 
-    rays_d_normalize: bool = True
+        if self.cfg.use_random_camera:
+            self.rand_cam_bs = self.cfg.random_camera.batch_size
+            self.cfg.random_camera.update(
+                {"batch_size": self.num_frames * self.rand_cam_bs}
+            )
+        self.setup(cfg, split)
+        # self.single_image_dataset = SingleImageIterableDataset(self.cfg, split)
 
-
-class SingleImageDataBase:
     def setup(self, cfg, split):
         self.split = split
         self.rank = get_rank()
-        self.cfg: SingleImageDataModuleConfig = cfg
+        self.cfg: TemporalRandomImageDataModuleConfig = cfg
 
         if self.cfg.use_random_camera:
             random_camera_cfg = parse_structured(
@@ -125,50 +124,26 @@ class SingleImageDataBase:
             assert len(self.heights) == len(self.cfg.resolution_milestones) + 1
             self.resolution_milestones = [-1] + self.cfg.resolution_milestones
 
-        self.directions_unit_focals = [
-            get_ray_directions(H=height, W=width, focal=1.0)
-            for (height, width) in zip(self.heights, self.widths)
-        ]
         self.focal_lengths = [
             0.5 * height / torch.tan(0.5 * self.fovy) for height in self.heights
         ]
 
         self.height: int = self.heights[0]
         self.width: int = self.widths[0]
-        self.directions_unit_focal = self.directions_unit_focals[0]
         self.focal_length = self.focal_lengths[0]
-        self.set_rays()
-        self.load_images()
+        self.load_video_frames()
         self.prev_height = self.height
 
-    def set_rays(self):
-        # get directions by dividing directions_unit_focal by focal length
-        directions: Float[Tensor, "1 H W 3"] = self.directions_unit_focal[None]
-        directions[:, :, :, :2] = directions[:, :, :, :2] / self.focal_length
+        self.timestamps = torch.arange(self.num_frames, dtype=torch.float32)
+        if self.cfg.norm_timestamp:
+            self.timestamps = self.timestamps / self.num_frames
 
-        rays_o, rays_d = get_rays(
-            directions,
-            self.c2w,
-            keepdim=True,
-            noise_scale=self.cfg.rays_noise_scale,
-            normalize=self.cfg.rays_d_normalize,
-        )
-
-        proj_mtx: Float[Tensor, "4 4"] = get_projection_matrix(
-            self.fovy, self.width / self.height, 0.1, 100.0
-        )  # FIXME: hard-coded near and far
-        mvp_mtx: Float[Tensor, "4 4"] = get_mvp_matrix(self.c2w, proj_mtx)
-
-        self.rays_o, self.rays_d = rays_o, rays_d
-        self.mvp_mtx = mvp_mtx
-
-    def load_images(self):
+    # Copied from threestudio.data.image.SingleImageDataBase.load_images
+    def load_single_frame(self, frame_path):
         # load image
-        assert os.path.exists(
-            self.cfg.image_path
-        ), f"Could not find image {self.cfg.image_path}!"
+        assert os.path.exists(frame_path), f"Could not find image {frame_path}!"
         rgba = cv2.cvtColor(
-            cv2.imread(self.cfg.image_path, cv2.IMREAD_UNCHANGED), cv2.COLOR_BGRA2RGBA
+            cv2.imread(frame_path, cv2.IMREAD_UNCHANGED), cv2.COLOR_BGRA2RGBA
         )
         rgba = (
             cv2.resize(
@@ -177,58 +152,112 @@ class SingleImageDataBase:
             / 255.0
         )
         rgb = rgba[..., :3]
-        self.rgb: Float[Tensor, "1 H W 3"] = (
+        rgb: Float[Tensor, "1 H W 3"] = (
             torch.from_numpy(rgb).unsqueeze(0).contiguous().to(self.rank)
         )
-        self.mask: Float[Tensor, "1 H W 1"] = (
+        self.rgbs.append(rgb)
+        mask: Float[Tensor, "1 H W 1"] = (
             torch.from_numpy(rgba[..., 3:] > 0.5).unsqueeze(0).to(self.rank)
         )
+        self.masks.append(mask)
         print(
-            f"[INFO] single image dataset: load image {self.cfg.image_path} {self.rgb.shape}"
+            f"[INFO] single image dataset: load image {frame_path} {self.rgb.shape}"
         )
 
         # load depth
         if self.cfg.requires_depth:
-            depth_path = self.cfg.image_path.replace("_rgba.png", "_depth.png")
+            depth_path = frame_path.replace("_rgba.png", "_depth.png")
             assert os.path.exists(depth_path)
             depth = cv2.imread(depth_path, cv2.IMREAD_UNCHANGED)
             depth = cv2.resize(
                 depth, (self.width, self.height), interpolation=cv2.INTER_AREA
             )
-            self.depth: Float[Tensor, "1 H W 1"] = (
+            depth: Float[Tensor, "1 H W 1"] = (
                 torch.from_numpy(depth.astype(np.float32) / 255.0)
                 .unsqueeze(0)
                 .to(self.rank)
             )
+            self.depths.append(depth)
             print(
-                f"[INFO] single image dataset: load depth {depth_path} {self.depth.shape}"
+                f"[INFO] single image dataset: load depth {depth_path} {depth.shape}"
             )
         else:
-            self.depth = None
+            depth = None
 
         # load normal
         if self.cfg.requires_normal:
-            normal_path = self.cfg.image_path.replace("_rgba.png", "_normal.png")
+            normal_path = frame_path.replace("_rgba.png", "_normal.png")
             assert os.path.exists(normal_path)
             normal = cv2.imread(normal_path, cv2.IMREAD_UNCHANGED)
             normal = cv2.resize(
                 normal, (self.width, self.height), interpolation=cv2.INTER_AREA
             )
-            self.normal: Float[Tensor, "1 H W 3"] = (
+            normal: Float[Tensor, "1 H W 3"] = (
                 torch.from_numpy(normal.astype(np.float32) / 255.0)
                 .unsqueeze(0)
                 .to(self.rank)
             )
+            self.normals.append(normal)
             print(
-                f"[INFO] single image dataset: load normal {normal_path} {self.normal.shape}"
+                f"[INFO] single image dataset: load normal {normal_path} {normal.shape}"
             )
         else:
-            self.normal = None
+            normal = None
+
+    
+    def load_video_frames(self):
+        assert os.path.exists(self.cfg.video_frames_dir), f"Could not find image {self.cfg.video_frames_dir}!"
+        self.rgbs = []
+        self.masks = []
+        if self.cfg.requires_depth:
+            self.depths = []
+        if self.cfg.requires_normal:
+            self.normals = []
+
+        for idx in range(self.cfg.num_frames):
+            frame_path = os.path.join(self.cfg.video_frames_dir, f"{idx:03}_rgba.png")
+            self.load_single_frame(frame_path)
+
+        self.rgbs = torch.cat(self.rgbs, dim=0)
+        self.masks = torch.cat(self.masks, dim=0)
+        if self.cfg.requires_depth:
+            self.depths = torch.cat(self.depths, dim=0)
+        if self.cfg.requires_normal:
+            self.normals = torch.cat(self.normals, dim=0)
+
 
     def get_all_images(self):
-        return self.rgb
+        return self.rgbs
 
-    def update_step_(self, epoch: int, global_step: int, on_load_weights: bool = False):
+    def collate(self, batch) -> Dict[str, Any]:
+
+        batch = {
+            # "rays_o": self.rays_o,
+            # "rays_d": self.rays_d,
+            # "mvp_mtx": self.mvp_mtx,
+            "camera_positions": self.camera_position,
+            "light_positions": self.light_position,
+            "elevation": self.elevation_deg,
+            "azimuth": self.azimuth_deg,
+            "camera_distances": self.camera_distance,
+            "rgb": self.rgbs,
+            "ref_depth": self.depths,
+            "ref_normal": self.normals,
+            "mask": self.masks,
+            "height": self.height,
+            "width": self.width,
+            "c2w": self.c2w4x4.repeat(self.num_frames, 1, 1),
+            "fovy": self.fovy.repeat(self.num_frames),
+            "timestamp": self.timestamps
+        }
+        if self.cfg.use_random_camera:
+            batch_rand_cam = self.random_pose_generator.collate(None)
+            batch_rand_cam["timestamp"] = self.timestamps.repeat_interleave(self.rand_cam_bs)
+            batch["random_camera"] = batch_rand_cam
+
+        return batch
+    
+    def update_step(self, epoch: int, global_step: int, on_load_weights: bool = False):
         size_ind = bisect.bisect_right(self.resolution_milestones, global_step) - 1
         self.height = self.heights[size_ind]
         if self.height == self.prev_height:
@@ -236,44 +265,10 @@ class SingleImageDataBase:
 
         self.prev_height = self.height
         self.width = self.widths[size_ind]
-        self.directions_unit_focal = self.directions_unit_focals[size_ind]
         self.focal_length = self.focal_lengths[size_ind]
         threestudio.debug(f"Training height: {self.height}, width: {self.width}")
-        self.set_rays()
-        self.load_images()
+        self.load_video_frames()
 
-
-class SingleImageIterableDataset(IterableDataset, SingleImageDataBase, Updateable):
-    def __init__(self, cfg: Any, split: str) -> None:
-        super().__init__()
-        self.setup(cfg, split)
-
-    def collate(self, batch) -> Dict[str, Any]:
-        batch = {
-            "rays_o": self.rays_o,
-            "rays_d": self.rays_d,
-            "mvp_mtx": self.mvp_mtx,
-            "camera_positions": self.camera_position,
-            "light_positions": self.light_position,
-            "elevation": self.elevation_deg,
-            "azimuth": self.azimuth_deg,
-            "camera_distances": self.camera_distance,
-            "rgb": self.rgb,
-            "ref_depth": self.depth,
-            "ref_normal": self.normal,
-            "mask": self.mask,
-            "height": self.height,
-            "width": self.width,
-            "c2w": self.c2w4x4,
-            "fovy": self.fovy,
-        }
-        if self.cfg.use_random_camera:
-            batch["random_camera"] = self.random_pose_generator.collate(None)
-
-        return batch
-
-    def update_step(self, epoch: int, global_step: int, on_load_weights: bool = False):
-        self.update_step_(epoch, global_step, on_load_weights)
         self.random_pose_generator.update_step(epoch, global_step, on_load_weights)
 
     def __iter__(self):
@@ -281,49 +276,21 @@ class SingleImageIterableDataset(IterableDataset, SingleImageDataBase, Updateabl
             yield {}
 
 
-class SingleImageDataset(Dataset, SingleImageDataBase):
-    def __init__(self, cfg: Any, split: str) -> None:
-        super().__init__()
-        self.setup(cfg, split)
-
-    def __len__(self):
-        return len(self.random_pose_generator)
-
-    def __getitem__(self, index):
-        return self.random_pose_generator[index]
-        # if index == 0:
-        #     return {
-        #         'rays_o': self.rays_o[0],
-        #         'rays_d': self.rays_d[0],
-        #         'mvp_mtx': self.mvp_mtx[0],
-        #         'camera_positions': self.camera_position[0],
-        #         'light_positions': self.light_position[0],
-        #         'elevation': self.elevation_deg[0],
-        #         'azimuth': self.azimuth_deg[0],
-        #         'camera_distances': self.camera_distance[0],
-        #         'rgb': self.rgb[0],
-        #         'depth': self.depth[0],
-        #         'mask': self.mask[0]
-        #     }
-        # else:
-        #     return self.random_pose_generator[index - 1]
-
-
-@register("single-image-datamodule")
-class SingleImageDataModule(pl.LightningDataModule):
-    cfg: SingleImageDataModuleConfig
+@register("temporal-image-datamodule")
+class TemporalRandomImageDataModule(pl.LightningDataModule):
+    cfg: TemporalRandomImageDataModuleConfig
 
     def __init__(self, cfg: Optional[Union[dict, DictConfig]] = None) -> None:
         super().__init__()
-        self.cfg = parse_structured(SingleImageDataModuleConfig, cfg)
+        self.cfg = parse_structured(TemporalRandomImageDataModuleConfig, cfg)
 
     def setup(self, stage=None) -> None:
         if stage in [None, "fit"]:
-            self.train_dataset = SingleImageIterableDataset(self.cfg, "train")
+            self.train_dataset = TemporalRandomImageIterableDataset(self.cfg, "train")
         if stage in [None, "fit", "validate"]:
-            self.val_dataset = SingleImageDataset(self.cfg, "val")
+            self.val_dataset = RandomCameraDataset(self.cfg.get("random_camera", {}), "val")
         if stage in [None, "test", "predict"]:
-            self.test_dataset = SingleImageDataset(self.cfg, "test")
+            self.test_dataset = RandomCameraDataset(self.cfg.get("random_camera", {}), "test")
 
     def prepare_data(self):
         pass
